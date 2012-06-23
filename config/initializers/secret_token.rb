9f28763babf63d65eb221e1a8eb73e4e18247e72
Jarrett Linowes <jarrett@linowes.com>
2012-06-23 13:46:13 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KRF::Application.config.secret_token = '417eaa52851ad5eb207bff972c3f87b9fee56f00e6050d4a049941b389c4af2d1dad908830905492ba23b546f0be6391720c8b1a0455879bac1701d29cbad296'
