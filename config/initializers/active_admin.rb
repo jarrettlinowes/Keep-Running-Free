bccab1209f638e41a73fdb1ee88e12518dc87fdf
Jonathan Linowes <linojon@gmail.com>
2012-07-29 00:14:54 -0400

    vendored active_admin assets, config auth for our simple authentication

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -55,7 +55,8 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # within the controller.
-  config.authentication_method = :authenticate_admin_user!
+  #config.authentication_method = :authenticate_admin_user!
+  config.authentication_method = :authorize 
 
 
   # == Current User
@@ -65,8 +66,8 @@ ActiveAdmin.setup do |config|
   #
   # This setting changes the method which Active Admin calls
   # to return the currently logged in user.
-  config.current_user_method = :current_admin_user
-
+  #config.current_user_method = :current_admin_user
+  config.current_user_method = :current_user
 
   # == Logging Out
   #
@@ -78,13 +79,14 @@ ActiveAdmin.setup do |config|
   # will call the method to return the path.
   #
   # Default:
-  config.logout_link_path = :destroy_admin_user_session_path
+  config.logout_link_path = 'sessions/current'
 
   # This setting changes the http method used when rendering the
   # link. For example :get, :delete, :put, etc..
   #
   # Default:
   # config.logout_link_method = :get
+  config.logout_link_method = :delete
 
 
   # == Admin Comments
