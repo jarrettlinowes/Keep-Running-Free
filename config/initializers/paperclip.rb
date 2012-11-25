ff69fcaecff366ed4fd721c84143f23c03dba3be
Jonathan Linowes <linojon@gmail.com>
2012-11-24 21:59:46 -0500

    set bucket in paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,6 +1,7 @@
 Paperclip::Attachment.default_options.merge!(
 	storage: :s3,
-	s3_credentials: "#{Rails.root}/config/s3.yml"
+	s3_credentials: "#{Rails.root}/config/s3.yml",
+	bucket: Rails.env == 'production' ? 'krf-dev' : 'krf-dev'
 )
 
 #to do: Use Heroku ENV for s3 https://devcenter.heroku.com/articles/uploading-files-s3-rails-paperclip
