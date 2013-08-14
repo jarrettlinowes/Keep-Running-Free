a0b5274c3f33975aa0503a2ac430bbb13005c58c
Jarrett Linowes <jarrett@linowes.com>
2013-08-14 14:33:45 -0400

    Home Page Welcome

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 	def home
 		@current_section = 'home'
 		@recent_feature = Post.p_featured.order("published_on DESC")
+		@authors = Author.all
 	end
 
 	def about
