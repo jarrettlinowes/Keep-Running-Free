5a52725bc4c632a0de3be181e60bb0664f507785
Jarrett Linowes <jarrett@linowes.com>
2013-08-21 18:13:46 -0400

    removed ckeditor from authors

--- app/admin/authors.rb
+++ app/admin/authors.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Author do
   form do |f|
   	f.inputs do
   		f.input :name
-  		f.input :about, :input_html => {:class => "ckeditor"}
+  		f.input :about
   		f.input :title_picture_id
   	end
   	f.buttons
