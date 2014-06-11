e0dc851f1479f0eae452b4706c5b470829bdf303
Jarrett Linowes <jarrett@linowes.com>
2014-06-11 17:01:28 -0400

    added secret key generator

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,4 +229,11 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  Devise.token_generator ||=
+    if secret_key = Devise.secret_key
+      Devise::TokenGenerator.new(
+        Devise::CachingKeyGenerator.new(Devise::KeyGenerator.new(secret_key))
+      )
+    else
+      raise <<-ERROR
 end
