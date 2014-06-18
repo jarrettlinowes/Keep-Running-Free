763ae1ffe27d0c84acca77c234ae715eea0b0da9
Jarrett Linowes <jarrett@linowes.com>
2014-06-18 16:37:51 -0400

    added paperclip image validation

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -5,7 +5,6 @@ class Ckeditor::AttachmentFile < Ckeditor::Asset
   
   validates_attachment_size :data, :less_than => 100.megabytes
   validates_attachment_presence :data
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	
 	def url_thumb
 	  @url_thumb ||= Ckeditor::Utils.filethumb(filename)
