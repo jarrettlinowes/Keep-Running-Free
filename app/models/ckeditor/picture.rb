f06bc53818b08250329fe3a0ac9dc7a31d8b33c8
Jarrett Linowes <jarrett@linowes.com>
2014-06-19 02:32:14 -0400

    reverted back to !

--- app/models/ckeditor/picture.rb
+++ app/models/ckeditor/picture.rb
@@ -2,7 +2,7 @@ class Ckeditor::Picture < Ckeditor::Asset
   has_attached_file :data,
                     :url  => "/pictures/:id/:style_:basename.:extension",
                     :path => "/pictures/:id/:style_:basename.:extension",
-	                  :styles => { :content => '625x285^', :flist => '429x196', :thumb => '175x80', :list => '175x80!', :feature => '700x319!', :front => '1031x469!', :sfeature => '327x149!' }
+	                  :styles => { :content => '625x285!', :flist => '429x196', :thumb => '175x80', :list => '175x80!', :feature => '700x319!', :front => '1031x469!', :sfeature => '327x149!' }
 	
 	validates_attachment_size :data, :less_than => 2.megabytes
 	validates_attachment_presence :data
