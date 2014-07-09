af78e88998b4e94d532cb9e68aec8170fd5e3890
Jarrett Linowes <jarrett@linowes.com>
2014-07-09 00:35:09 -0400

    added nav image/precompile

--- public/assets/ckeditor/plugins/devtools/lang/da.js
+++ public/assets/ckeditor/plugins/devtools/lang/da.js
@@ -2,4 +2,6 @@
 Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang("devtools","da",{devTools:{title:"Information på elementet",dialogName:"Dialogboks",tabName:"Tab beskrivelse",elementId:"ID på element",elementType:"Type af element"}});
+
+
+CKEDITOR.plugins.setLang('devtools','da',{devTools:{title:'Information på elementet',dialogName:'Dialogboks',tabName:'Tab beskrivelse',elementId:'ID på element',elementType:'Type af element'}});
--- public/assets/ckeditor/plugins/docprops/plugin.js
+++ public/assets/ckeditor/plugins/docprops/plugin.js
@@ -2,4 +2,6 @@
 Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.add("docprops",{init:function(o){var d=new CKEDITOR.dialogCommand("docProps");d.modes={wysiwyg:o.config.fullPage},o.addCommand("docProps",d),CKEDITOR.dialog.add("docProps",this.path+"dialogs/docprops.js"),o.ui.addButton("DocProps",{label:o.lang.docprops.label,command:"docProps"})}});
+
+
+CKEDITOR.plugins.add('docprops',{init:function(a){var b=new CKEDITOR.dialogCommand('docProps');b.modes={wysiwyg:a.config.fullPage};a.addCommand('docProps',b);CKEDITOR.dialog.add('docProps',this.path+'dialogs/docprops.js');a.ui.addButton('DocProps',{label:a.lang.docprops.label,command:'docProps'});}});
