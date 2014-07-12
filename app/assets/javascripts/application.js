de5efea16294df74f2861e60e376c00f94020bdf
Jarrett Linowes <jarrett@linowes.com>
2014-07-12 00:57:58 -0400

    started mobile nav and precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
+//= require bootstrap.min
 //= require ckeditor/init
 //= require_tree .
 
--- app/assets/javascripts/ckeditor.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-   if ($('#active_admin_content textarea').length > 0) {       
-     var data = $('textarea');
-     $.each(data, function(i) {
-       CKEDITOR.replace(data[i].id);
-     });     
-   }  
-});
